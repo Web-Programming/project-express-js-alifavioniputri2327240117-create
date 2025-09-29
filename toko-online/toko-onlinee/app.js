ac46c5fee7c9bdb674f86211b5a79909272f2de5
achmadsyamiri2611 <achmadsyamiri_2327240099@mhs.mdp.ac.id>
2025-09-29 16:59:46 +0700

    pertemuan5

--- toko-online/toko-onlinee/app.js
+++ toko-online/toko-onlinee/app.js
@@ -4,13 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//Perbaikan ke 2
+var indexRouter = require('./app_toko_online/routes/index');
+var usersRouter = require('./app_toko_online/routes/users');
+var productRouter = require("./app_toko_online/routes/product");
+var engine = require('ejs-blocks'); //menggunakan ejs block
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_toko_online', 'views'));
+app.engine('ejs', engine); //daftarkan engine ejs block
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//serving boostrap
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -26,7 +30,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-var productRouter = require("./routes/product"); //letakan diatas agar rapi
+var productRouter = require("./app_toko_online/routes/product"); //letakan diatas agar rapi
 app.use("/produk", productRouter);
 
 // error handler
